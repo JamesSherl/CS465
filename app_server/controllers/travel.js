81723918a8ba4f3eecb9e24fab7905c5d53996da
James Sherlin <james.sherlin@snhu.edu>
2025-07-12 19:30:32 -0400

    Module 3 completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /*Traval view time :D*/
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaway'} );
+    res.render('travel', {title: 'Travlr Getaway', trips} );
 };
 
 module.exports = {
