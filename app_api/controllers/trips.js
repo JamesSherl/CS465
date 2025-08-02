e7d1c5ad96ca6761a8253119e44f9ae5aaaec152
James Sherlin <james.sherlin@snhu.edu>
2025-08-01 21:49:11 -0400

    Module 5 completed Baseline

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); //Registers model
+const Model = mongoose.model('trips');
+
+//GET: /trips - lists all the trips
+//Regardless of outcome, response must include HTML status codes
+// and JSON message to the requesting client
+const tripsList = async(req, res) => {
+    const q = await Model
+        .find({}) //Returns ALL records
+        .exec();
+
+
+        //Uncomment the following line to show results of the query on the console
+        // console.log(q);
+
+        if(!q)
+        { //Database returns no data
+            return res
+                .status(404)
+                .json(err);
+        } else{ //Returns trip list
+            return res  .status(200)
+            .json(q);
+        
+
+        }
+
+};
+const tripsFindByCode = async(req, res) => {
+    const q = await Model
+        .find({'code' : req.params.tripCode }) //Returns single records
+        .exec();
+
+
+        //Uncomment the following line to show results of the query on the console
+        // console.log(q);
+
+        if(!q)
+        { //Database returns no data
+            return res
+                .status(404)
+                .json(err);
+        } else{ //Returns trip list
+            return res  .status(200)
+            .json(q);
+        
+
+        }
+
+};
+
+module.exports = {
+    tripsList,
+    tripsFindByCode
+};
